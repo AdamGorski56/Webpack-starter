1cac8114813cc507c108af7e44ed0a6ce5252cd3
Test <justtest@gmail.com>
2020-10-12 15:13:45 +0200

    webpack starter done

---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+
+
+
+module.exports = {
+    mode: "production", 
+    entry: {
+      main: "./src/index.js",
+      libs: "./src/libs.js",
+    },
+    
+    output: {
+        filename: "[name].[contenthash].js",
+        path: path.resolve(__dirname, "dist")
+    },
+
+    module: {
+        rules: [
+         
+            {
+              test: /\.js$/,
+              exclude: /node_modules/,
+              use: {
+                loader: 'babel-loader',
+              }
+            },
+  
+          {
+            test: /\.html$/i,
+            loader: 'html-loader',
+            options: { minimize: true }
+          },
+          
+          {
+            test: /\.scss$/i,
+            use: [
+                MiniCssExtractPlugin.loader,
+                'css-loader',
+                'sass-loader'
+            ],
+          },
+          {
+            test: /\.(png|jpe?g|gif|svg)$/i,
+            use: [
+              {
+                loader: 'file-loader',
+              },
+            ],
+          },
+        ],
+      },
+
+      plugins : [
+
+        new HtmlWebPackPlugin({
+          template: "./src/index.html",
+          filename: "./index.html"
+        }),
+
+        new MiniCssExtractPlugin({
+          filename: '[name].[contenthash].css',
+          chunkFilename: '[id].[contenthash].css',
+        }),
+
+        new CleanWebpackPlugin({}),
+
+      ]
+}
